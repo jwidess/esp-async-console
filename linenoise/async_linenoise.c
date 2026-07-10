e3f81aa39a27af8a3814e0342a620af5212c27ef
Justin Widen <jdogwiden5@gmail.com>
2026-07-10 00:43:45 -0500

    fix: async log redraw active tab completion
    Modified the linenoise completeLine blocking loop to keep global state struct pointing at active completion string.
    Before, the struct was restored to the uncompleted buffer, causing async linenoiseShow to redraw the raw incomplete buffer instead of the completion hint that was displayed.

--- linenoise/async_linenoise.c
+++ linenoise/async_linenoise.c
@@ -435,38 +435,45 @@ static int completeLine(struct linenoiseState *ls) {
         size_t stop = 0, i = 0;
 
         while(!stop) {
+            struct linenoiseState saved = *ls;
+
             /* Show completion or original buffer */
             if (i < lc.len) {
-                struct linenoiseState saved = *ls;
-
                 ls->len = ls->pos = strlen(lc.cvec[i]);
                 ls->buf = lc.cvec[i];
-                refreshLine(ls);
-                ls->len = saved.len;
-                ls->pos = saved.pos;
-                ls->buf = saved.buf;
-            } else {
-                refreshLine(ls);
             }
+            refreshLine(ls);
 
             nread = read_func(in_fd, &c, 1);
             if (nread <= 0) {
+                ls->len = saved.len;
+                ls->pos = saved.pos;
+                ls->buf = saved.buf;
                 freeCompletions(&lc);
                 return -1;
             }
 
             switch(c) {
                 case TAB: /* tab */
+                    ls->len = saved.len;
+                    ls->pos = saved.pos;
+                    ls->buf = saved.buf;
                     i = (i+1) % (lc.len+1);
                     if (i == lc.len) linenoiseBeep();
                     break;
                 case ESC: /* escape */
+                    ls->len = saved.len;
+                    ls->pos = saved.pos;
+                    ls->buf = saved.buf;
                     /* Re-show original buffer */
                     if (i < lc.len) refreshLine(ls);
                     stop = 1;
                     break;
                 default:
                     /* Update buffer and return */
+                    ls->len = saved.len;
+                    ls->pos = saved.pos;
+                    ls->buf = saved.buf;
                     if (i < lc.len) {
                         nwritten = snprintf(ls->buf,ls->buflen,"%s",lc.cvec[i]);
                         ls->len = ls->pos = nwritten;
